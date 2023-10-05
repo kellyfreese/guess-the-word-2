9d457fb67c45cb748cd017fdf61ad2912f2fa7e8
Kelly F <95229425+kellyraefoote@users.noreply.github.com>
2023-10-05 11:13:20 -0700

    Added checkIfWin

--- js/script.js
+++ js/script.js
@@ -51,6 +51,41 @@ const makeGuess = function (guess) {
         message.innerText = "You already guessed that letter, silly. Try again.";
     } else {
         guessedLetters.push(guess);
-        console.log(guessedLetters);
+        showGuessedLetters();
+        updateWordInProgress(guessedLetters);
     }
+
 } ;
+
+const showGuessedLetters = function () {
+    guessedLettersElement.innerHTML = ""; 
+    for (const letter of guessedLetters) {
+        const li = document.createElement ("li");
+        li.innerText = letter;
+        guessedLettersElement.append(li);
+    }
+    
+};
+
+const updateWordInProgress = function (guessedLetters) {
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    console.log(wordArray);
+    const revealWord = [];
+    for (const letter of wordArray) {
+        if (guessedLetters.includes(letter)) {
+            revealWord.push(letter.toUpperCase());
+        } else {
+            revealWord.push("●");
+        }
+    }
+    wordInProgress.innerText = revealWord.join("");
+    checkIfWin();
+};
+
+const checkIfWin = function() {
+    if (word.toUpperCase() === wordInProgress.innerText) {
+        message.classList.add("win");
+        message.innerHTML = `<p class="highlight">You guessed correct the word! Congrats!</p>`;
+    }
+};
